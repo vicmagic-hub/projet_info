c3c85aed30838d8eaa17431947eb63dbf39b00fd
vicmagic-hub <poubellesecretepub@gmail.com>
2026-04-14 18:39:47 +0200

    creation du lien piece - echiquier pour les positions

--- model/board.py
+++ model/board.py
@@ -1,11 +1,10 @@
 class Board:
     def __init__(self):
-        self.board = [[None for _ in range(8)] for _ in range(8)]
+        self.squares = [[None for _ in range(8)] for _ in range(8)]
     
     def test_case(self, position):
         """
         Méthode pour tester si une case est occupée ou non
         """
         i, j = position
-        return self.board[i][j] != None
-    
+        return self.squares[i][j] != None
--- model/piece.py
+++ model/piece.py
@@ -1,12 +1,14 @@
-import board
+from board import Board
 
 class Piece:
     """
     Classe de base pour les pièces d'échecs
     """
-    def __init__(self,color, position):
+    def __init__(self,color, position, board):
         self.color = color
-        self.position = position 
+        self.board = board
+        self.position = position
+        self.board.squares[position[0]][position[1]] = self
         self.marque = ''
 
     def __str__(self):
@@ -15,40 +17,29 @@ class Piece:
         return self.marque + col + str(i+1)
     
     def move(self, new_position):
-        """
-        Méthode pour déplacer la pièce à une nouvelle position
-        Pour le moment elle est très con
-        Mais bientôt elle sera abstraite
-        Chaque pièce aura sa méthode de déplacement spécifique
-        """
+        self.board.squares[self.position[0]][self.position[1]] = None
+        self.board.squares[new_position[0]][new_position[1]] = self
         self.position = new_position
     
     def take(self, new_position):
-        """
-        Méthode pour prendre une pièce adverse
-        Pour le moment elle est très con
-        Mais bientôt elle sera abstraite
-        Chaque pièce aura sa méthode de prise spécifique
-        """
+        self.board.squares[self.position[0]][self.position[1]] = None
+        self.board.squares[new_position[0]][new_position[1]] = self
         self.position = new_position
     
     def possible_moves(self):
-        """
-        Méthode pour obtenir les mouvements possibles de la pièce
-        Pour le moment elle est très con
-        Mais bientôt elle sera abstraite
-        Chaque pièce aura sa méthode de mouvements possibles spécifique
-        """
+        #future classe abstraite, à implémenter pour chaque type de pièce
         return []
     
 class Pawn(Piece):
-    def __init__(self, color, position):
-        super().__init__(color, position)
-        self.marque = 'p'
+    def __init__(self, color, position, board):
+        super().__init__(color, position, board)
+        self.marque = ''
         self.first_move = True
     
     def possible_moves(self):
         #ne gère pas les colisions/sorties de plateau pour le moment
+        #ne gère pas les prises pour le moment
+        #ne gère pas la promotion pour le moment
         """
         Méthode pour obtenir les mouvements possibles du pion
         Renvoie une liste de futures positions possibles pour le pion
@@ -94,10 +85,14 @@ class King(Piece):
         self.first_move = True
 
 
-a=board.Board()
-p = Pawn('white', (1,0))
-a.board[1][0] = p
-print(p.possible_moves())
-print(p, p.position)
-p.move((3,0))
-print(p, p.position)
+#tests temporaires
+if __name__ == "__main__":
+    a=Board()
+    p = Pawn('white', (1,0), a)
+    print(p.possible_moves())
+    print(p, p.position)
+    print(a.test_case((1,0)))
+    p.move((3,0))
+    print(p, p.position)
+    print(a.test_case((3,0)))
+    print(p.possible_moves())

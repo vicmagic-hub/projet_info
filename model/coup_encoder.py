f4ea8cfc7cda0536f3ebfba36dd5c24fbb68c71b
vicmagic-hub <poubellesecretepub@gmail.com>
2026-05-13 13:36:25 +0200

    ajout de l'interpreteur de coup

--- model/coup_encoder.py
+++ model/coup_encoder.py
@@ -6,7 +6,7 @@ class Move():
         """
         initialisation d'un coup : 
         entrées :   pièce concernée, position de départ, position d'arrivée, 
-                    type de coup :str parmi('normal', 'prise', 'enpassant', 'promotion', 'promoprise', 'castle', 'doublepion'),
+                    type de coup :str parmi('classic', 'prise', 'enpassant', 'promotion', 'promoprise', 'castle', 'doublepion'),
                     éventuelle pièce capturée
         garde aussi une marque pour la pièce de promotion, des flags échec/mat
         intègre les informations en self.prev_... pour revenir à l'état avant le coup
@@ -26,10 +26,119 @@ class Move():
         self.prev_white_king = None
         self.prev_black_king = None
 
+    @classmethod
+    def from_str (cls, s, color, board) :
+        """
+        création de coup en "lecture" depuis une string au format alternatif
+        entrée : string du coup, couleur du joueur
+        renvoie une instance de move correspondante
+        """
+        n = len (s)
+        captured_piece = None
+        promotion_piece = None
+        if s[:3] == "O-O" : 
+            #petit roque
+            if color == 'white' : 
+                piece = board.squares[0][4]
+                depart = (0,4)
+                arrivee = (0,6)
+            else :
+                piece = board.squares[7][4]
+                depart = (7,4)
+                arrivee = (7,6)
+            type = 'castle'
+            n_parcouru = 2
+        elif s[:5] =="O-O-O" :
+            #grand roque
+            if color == 'white' : 
+                piece = board.squares[0][4]
+                depart = (0,4)
+                arrivee = (0,2)
+            else :
+                piece = board.squares[7][4]
+                depart = (7,4)
+                arrivee = (7,2)
+            type = 'castle'
+            n_parcouru = 4
+        else : 
+            if s[0].isupper() : 
+                #coup de pièce
+                i,j = int(s[2]) - 1, ord(s[1])- ord('a')
+                piece = board.squares[i][j]
+                depart = i,j
+                if s[3] ==  'x' : 
+                    #prise
+                    type = 'prise'
+                    k,l = int(s[5]) - 1, ord(s[4])- ord('a')
+                    captured_piece = board.squares[k][l]
+                    n_parcouru = 5
+                else :
+                    type = 'classic'
+                    k,l = int(s[6]) - 1, ord(s[5])- ord('a')
+                    n_parcouru = 6
+                arrivee = (k,l)
+            else : 
+                #coup de pion
+                i,j = int(s[1]) - 1, ord(s[0])- ord('a')
+                piece = board.squares[i][j]
+                depart = i,j
+                if s[2] == 'x' : 
+                    #prise/promoprise/enpassant : 
+                    k,l = int(s[4]) - 1, ord(s[3])- ord('a')
+                    arrivee = k,l
+                    if color == 'white' :  prom_row = 7 
+                    else : prom_row = 0
+                    if k == prom_row : 
+                        #promoprise
+                        type = 'promoprise'
+                        promotion_piece = s[6]
+                        n_parcouru = 6
+                    elif not board.test_case((k,l)) :
+                        #en passant
+                        type = 'enpassant'
+                        if color == 'white' : captured_piece = board.squares[k][l-1]
+                        else : captured_piece = board.squares[k][l+1]
+                        n_parcouru = 4
+                    else : 
+                        #prise classique
+                        type = 'prise' 
+                        captured_piece = board.squares[k][l]
+                        n_parcouru = 4
+                else : 
+                    #promotion/classic/double-coup
+                    k,l = int(s[5]) - 1, ord(s[4])- ord('a')
+                    arrivee = k,l
+                    if color == 'white' :  prom_row = 7 
+                    else : prom_row = 0
+                    if k == prom_row : 
+                        #promoprise
+                        type = 'promotion'
+                        promotion_piece = s[7]
+                        n_parcouru = 7
+                    elif abs(k-i) == 2 :
+                        #double coup
+                        type = 'doublepion'
+                        n_parcouru = 5
+                    else : 
+                        #coup normal
+                        type = 'classic'
+                        n_parcouru = 5
+        #generation du coup
+        m = cls (piece, depart, arrivee, type, captured_piece)
+        m.promotion_piece = promotion_piece
+        if n_parcouru + 1 < n :
+            if s[-1] == '+' : 
+                m.is_a_check = True
+            else :
+                m.is_a_mat = True
+                m.is_a_check = True
+        return m
+             
+
     def __str__(self):
         """
         Affichage d'un coup en notation non ambiguë
-        renvoie "Na4->b6" pour un coup classique, "e4*d5" pour une prise, "e7->e8=Q" pour une promotion, "O-O" pour un petit roque, etc
+        renvoie "Na4->b6" pour un coup classique, "e4xd5" pour une prise, "e7->e8=Q" pour une promotion, "O-O" pour un petit roque, etc
         """
         piece = self.piece
         i,j = self.depart
